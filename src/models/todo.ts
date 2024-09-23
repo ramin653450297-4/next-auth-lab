43e965a7f5ff01a008d4f3f55aea1341769198de
ramin653450297-4 <119285401+ramin653450297-4@users.noreply.github.com>
2024-09-23 13:14:33 +0700

    main

---
+++ src/models/todo.ts
@@ -0,0 +1,14 @@
+import { Description } from "@mui/icons-material";
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    status: Boolean,
+    duedate: String,
+
+
+  });
+//connent to "todos" collection
+const Todo = mongoose.models.Todo || mongoose.model("Todo", TodoSchema);
+export default Todo;  
